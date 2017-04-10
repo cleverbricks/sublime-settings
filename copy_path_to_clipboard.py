79bdad479f3c8372f08cdbe80820f2c9e6dd01ab
cleverbricks <mudras@cleverbricks.com>
2017-04-10 02:06:19 -0400

    ST3 Macro - Copy to Clipboard

--- copy_path_to_clipboard.py
+++ copy_path_to_clipboard.py
@@ -8,3 +8,4 @@ class CopyPathToClipboard(sublime_plugin.TextCommand):
         line_number, column = self.view.rowcol(self.view.sel()[0].begin())
         line_number += 1
         sublime.set_clipboard(self.view.file_name() + ':' + str(line_number))
+    
